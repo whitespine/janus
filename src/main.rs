06a165c051eea6c1f47b4c050469a57f2f0b3195
WHITESPINE <jakehenry,tn@gmail.com>
2025-05-03 22:51:47 -0400

    Able to get basic meta information

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::thread::sleep;
+use std::time::Duration;
 use clap::Parser;
 use rust_socketio::{TransportType};
 use url::Url;
@@ -12,67 +14,65 @@ struct Args {
     #[arg(long)]
     host: String,
 
+    #[arg(long, action = clap::ArgAction::Count)]
+    add_session: u8,
     // Number of times to greet
     // #[arg(short, long, default_value_t = 1)]
     // count: u8,
 }
 
-fn main() {
+fn on_ack(evt: &str, payload: Payload, _socket: RawClient) {
+    println!("Ack {:?}", evt);
+    print_payload(payload)
+}
+
+fn print_payload(payload: Payload) {
+    #[allow(deprecated)] match payload {
+        Payload::Text(values) => println!("{:#?}", values),
+        Payload::Binary(bin_data) => println!("{:#?}", bin_data),
+        Payload::String(str) => println!("{:#?}", str),
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>>  {
     let args = Args::parse();
 
-    // define a callback which is called when a payload is received
-    // this callback gets the payload as well as an instance of the
-    // socket to communicate with the server
-    let callback = |payload: Payload, socket: RawClient| {
-        #[allow(deprecated)] match payload {
-            Payload::Text(values) => println!("Received: {:#?}", values),
-            Payload::Binary(bin_data) => println!("Received bytes: {:#?}", bin_data),
-            // This payload type is deprecated, use Payload::Text instead
-            Payload::String(str) => println!("Received: {}", str),
-        }
-        socket.emit("test", json!({"got ack": true})).expect("Server unreachable")
-    };
+    let mut session = None;
+    if args.add_session > 0 {
+        let response = reqwest::blocking::get(format!("{}/join", args.host))?;
+        session = Some(response.cookies().find(|cookie| cookie.name() == "session").expect("Failed to find session cookie").value().to_string());
+    }
 
-    // get a socket that is connected to the admin namespace
+    // Build a url with session if specified
     let mut url = Url::parse(&args.host).expect("Invalid URL");
-    url.path_segments_mut().unwrap().push("socket.io/");
-    url.query_pairs_mut().append_pair("session", "70ac46975b48d030bae1f821");
-    ClientBuilder::new(url)
+    if let Some(session) = session {
+        url.path_segments_mut().unwrap().push("socket.io/"); // Technically should be out of here, but this flag is stupid anyway
+        url.query_pairs_mut().append_pair("session", &session);
+    }
+
+    // Establish our client
+    println!("Connecting to url {:?}", url);
+    let socket = ClientBuilder::new(url)
         .transport_type(TransportType::Websocket)
         .reconnect(true)
         .reconnect_on_disconnect(true)
         .reconnect_delay(500, 500)
         .max_reconnect_attempts(10)
-        .on("test", callback)
-        .on("error", |err, _| eprintln!("Received Error: {:#?}", err))
+        .on_any(|evt, p, _| {
+            println!("Event received {:#?}", evt);
+            print_payload(p)
+        })
         .connect()
         .expect("Connection error");
 
 
+    // Say hello
+    socket.emit_with_ack("getJoinData", Payload::Text(vec![]), Duration::from_secs(2), |p, s| on_ack("getJoinData", p, s))
+         .expect("Server unreachable");
 
-/*
-
-        // .expect("Connection failed");
-
-    // emit to the "foo" event
-    let json_payload = json!({"token": 123});
-    socket.emit("foo", json_payload).expect("Server unreachable");
-
-    // define a callback, that's executed when the ack got acked
-    let ack_callback = |message: Payload, _| {
-        println!("Yehaa! My ack got acked?");
-        println!("Ack data: {:#?}", message);
-    };
-
-    let json_payload = json!({"myAckData": 123});
-    // emit with an ack
-    socket
-        .emit_with_ack("test", json_payload, Duration::from_secs(2), ack_callback)
-        .expect("Server unreachable");
-
-    socket.disconnect().expect("Disconnect failed")
-
- */
+    sleep(Duration::from_secs(5));
+    socket.disconnect().expect("Disconnect failed");
+    Ok(())
 }
 
 // Notes: game = Game.create used for in game view, and Setup.create used for auth, license, setup, etc
