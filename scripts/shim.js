1f5c8193dd5e4dfa3900668a15f13e4cf763afcb
WHITESPINE <jakehenry,tn@gmail.com>
2025-05-31 01:41:37 -0400

    Shimmy me timbers

--- scripts/shim.js
+++ scripts/shim.js
@@ -1,20 +1,62 @@
 // shim.js
-const { core } = Deno;
 
-function argsToMessage(...args) {
-    return args.map((arg) => JSON.stringify(arg)).join(" ");
+class MyFormData {
+
 }
 
-globalThis.console = {
-    log: (...args) => {
-        core.print(`[out]: ${argsToMessage(...args)}\n`, false);
-    },
-    warn: (...args) => {
-        core.print(`[warn]: ${argsToMessage(...args)}\n`, true);
-    },
-    error: (...args) => {
-        core.print(`[err]: ${argsToMessage(...args)}\n`, true);
-    },
-};
+globalThis.FormData = MyFormData;
 
-globalThis.logger = console;
+
+class CSSStyleDeclaration {
+    getPropertyValue() {
+        return 0; // IDK, figure this out later
+    }
+}
+
+class Element {
+    constructor() {
+        this.style = new CSS2Properties();
+    }
+}
+
+class HTMLElement extends Element { }
+
+class CSS2Properties { }
+
+// I'm not really sure what this thing is or does
+class HTMLHtmlElement extends HTMLElement { }
+
+class HTMLDocument {
+    constructor() {
+        this.documentElement = new HTMLHtmlElement();
+    }
+
+    createElement(element, options=null) {
+        return new Element(); // Todo, maybe more specificity?
+    }
+}
+
+class DOMParser { }
+class PIXIShader { }
+class PIXIContainer { }
+class PIXIColor { }
+class PIXIPoint { }
+class PIXIMesh { }
+class PIXIPolygon { }
+
+globalThis.document = new HTMLDocument();
+globalThis.getComputedStyle = (...args) => new CSSStyleDeclaration();
+globalThis.DOMParser = DOMParser;
+globalThis.HTMLElement = HTMLElement;
+globalThis.HTMLHtmlElement = HTMLHtmlElement;
+globalThis.PIXI = {
+    Shader: PIXIShader,
+    Container: PIXIContainer,
+    Color: PIXIColor,
+    Point: PIXIPoint,
+    Mesh: PIXIMesh,
+    Polygon: PIXIPolygon,
+    settings: {
+        PRECISION_VERTEX: null,
+    }
+}
--- src/script.rs
+++ src/script.rs
@@ -1,11 +1,21 @@
+use std::fs::read_to_string;
 use crate::error::ScriptError;
 use crate::Args;
 use rustyscript::{json_args, Error, Module, ModuleHandle, Runtime, RuntimeOptions};
 use std::time::Duration;
+use clap::Arg;
 
 fn vtt_path(args: &Args, subpath: &str) -> std::path::PathBuf {
     let vtt = std::path::Path::new(&args.vtt).join(subpath);
     std::path::absolute(vtt).expect("Bad Path") // We don't do this dynamically
+}
+
+fn vtt_module(args: &Args, name: &str, subpath: &str) -> Module {
+    let vtt_path = format!("{}/public/scripts/{}", args.vtt, subpath);
+    Module::new(
+        name,
+        read_to_string(vtt_path).expect(&format!("Failed to read path {}", subpath)),
+    )
 }
 
 pub struct FoundryRuntime {
@@ -14,15 +24,20 @@ pub struct FoundryRuntime {
 }
 
 impl FoundryRuntime {
-    pub fn new(_args: &Args, tokio_runtime: std::rc::Rc<tokio::runtime::Runtime>) -> FoundryRuntime {
+    pub fn new(args: &Args, tokio_runtime: std::rc::Rc<tokio::runtime::Runtime>) -> FoundryRuntime {
         let mut runtime = Runtime::with_tokio_runtime(RuntimeOptions {
             timeout: Duration::from_millis(500), // Stop execution by force after 50ms
             ..Default::default()
         }, tokio_runtime).expect("Failed to create runtime");
-        let core = Module::new(
-            "main.js", include_str!("../scripts/main.js")
-        );
-        let core_handle = runtime.load_module(&core).expect("Failed to load core");
+
+        // Load our core extension and foundry capabilities
+        let core = Module::new( "main.js", include_str!("../scripts/main.js"));
+        let shim = Module::new("shim.js", include_str!("../scripts/shim.js"));
+        let foundry = vtt_module(args, "foundry.mjs", "foundry.mjs");
+        let vendor = vtt_module(args, "./vendor.mjs", "vendor.mjs");
+        let core_handle = runtime.load_modules(&core, vec![&shim, &vendor, &foundry]).expect("Failed to load core");
+
+        // Load our foundry extension
         FoundryRuntime {
             core: core_handle,
             runtime
